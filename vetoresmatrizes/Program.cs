1ed153ea93a46d424589d189bccea2b22bd6753b
VICTOR PAULO CARVALHO RIBEIRO <victor.pcribeiro@senacsp.edu.br>
2025-11-18 17:11:35 -0300

    aula 18-11

---
+++ vetoresmatrizes/Program.cs
@@ -0,0 +1,46 @@
+﻿//Declarando dois vetores
+int[]numeros ={47, 6, 9, 43,5,19,15,23,8,7,49,50,51,12,53,84,56,27,58,95 }; // vetores inteiros
+string[] nomes = { "Claudio", "Ramiro", "Ester", "Felipe"}; // vetores de string
+
+
+Console.WriteLine("Vetor de numeros usando for:"); 
+for (int i = 0; i < numeros.Length; i++)//Exibindo os elementos do vetor    
+{
+    Console.WriteLine(numeros[i]);
+}
+
+Console.WriteLine("\nVetor de Nomes (usando foreach):"); 
+foreach (var nome in nomes) //Exibindo os Elementos do vetor de nomes com loop foreach
+{
+    Console.WriteLine(nome);
+}
+
+//Ordenação de vetores                                                     
+/*Array.Sort(numeros);
+Console.WriteLine("\nVetor de Numeros Ordenado");
+foreach(var numero in numeros)  //Exibindo os Elementos do vetor de nomes com loop foreach
+{
+    Console.WriteLine(numero);
+}
+*/
+int ValorProcurado = 88;
+int posicaosequencial = PesquisaSequencial(numeros, ValorProcurado);
+
+if (posicaosequencial != -1)
+{
+    Console.WriteLine($"\nPesquisa Sequencial : Valor {ValorProcurado} encontrado na posição {posicaosequencial}.");
+
+}
+else
+{
+    Console.WriteLine($"\nPesquisa Sequencial : Valor {ValorProcurado} não encontrado");
+}
+static int PesquisaSequencial(int[] vetor, int valor)
+{
+    for (int i = 0; i < vetor.Length;i++)
+    {
+        if(vetor[i] == valor)
+            return i;   //Retorna o Índice do Valor encontrado 
+    }
+    return -1; // Retorna -1 se não encontrar
+}
